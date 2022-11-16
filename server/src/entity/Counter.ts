4b86886d0db4bbd7e6f4f1429f83c61d77ebf6f0
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-11-16 18:38:28 +0100

    Creation of CounterResolver with a getAll and a create 👍

--- server/src/entity/Counter.ts
+++ server/src/entity/Counter.ts
@@ -1,6 +1,8 @@
-import {Field, ObjectType} from "type-graphql";
-import {Column, PrimaryGeneratedColumn} from "typeorm";
+import {Field, InputType, ObjectType} from "type-graphql";
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {MaxLength} from "class-validator";
 
+@Entity()
 @ObjectType()
 class Counter {
     @Field()
@@ -11,5 +13,11 @@ class Counter {
     @Column({length: 100})
     name: string;
 }
+
+@InputType()
+export class CounterInput {
+    @Field()
+    @MaxLength(100)
+    name: string;}
 
 export default Counter
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,39 +1,27 @@
 import "reflect-metadata";
-import express from 'express';
-import cors from 'cors';
+import datasource from "./db";
 
-import {config} from "dotenv";
-
-config()
-
-
-const app = express();
-
-app.use(express.json());
-app.use(cors({origin: process.env.FRONT_DEV_URL}));
-
+import {ApolloServerPluginLandingPageLocalDefault} from "apollo-server-core";
+import {ApolloServer} from "apollo-server";
+import {buildSchema} from "type-graphql";
+import {CounterResolver} from "./resolvers/CounterResolver";
 
 const start = async (): Promise<void> => {
-    console.log("Connecting to database ...");
-//
-//   await dataSource.initialize();
-//
-//   const schema = await buildSchema({
-//     // resolvers: [WilderResolver],
-//   });
-//
-//   const server = new ApolloServer({
-//     schema,
-//     csrfPrevention: true,
-//     cache: "bounded",
-//     plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
-//   });
+  await datasource.initialize();
 
-    // await server.listen().then(({ url }) => {
-    //   console.log(`🚀  Server is very ready at ${url}`);
-    // });
-    app.listen(5001, () => {
-        console.log("listening on port 5001");
+  const schema = await buildSchema({
+    resolvers: [CounterResolver],
+  });
+
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    cache: "bounded",
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  });
+
+    await server.listen().then(({ url }) => {
+      console.log(`💻 GQL Playground on ${url} 💻`);
     });
 };
 
