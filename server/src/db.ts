83274abd0050ba35e6f6c86d3f55b3f04f3b6d55
Sandra VOISIN <sandra.voisin92@gmail.com>
2022-11-17 14:19:16 +0100

    add entity to db

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,14 +1,15 @@
-import {DataSource} from "typeorm";
-import Counter from "./entity/Counter";
+import { DataSource } from 'typeorm';
+import Counter from './entity/Counter';
+import WaitingRoom from './entity/WaitingRoom';
 
 export default new DataSource({
-    type: "postgres",
-    host: "db",
-    port: 5432,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    synchronize: true,
-    entities: [Counter],
-    logging: ["error"],
+  type: 'postgres',
+  host: 'db',
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
+  entities: [Counter, WaitingRoom],
+  logging: ['error'],
 });
