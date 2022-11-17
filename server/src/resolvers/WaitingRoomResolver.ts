c1c31459f131c6b33a013a210b4a4efe44872f66
Sandra VOISIN <sandra.voisin92@gmail.com>
2022-11-17 11:48:56 +0100

    add waiting room route

---
+++ server/src/resolvers/WaitingRoomResolver.ts
@@ -0,0 +1,25 @@
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import WaitingRoom, { WaitingRoomInput } from '../entity/WaitingRoom';
+import dataSource from '../db';
+
+@Resolver(WaitingRoom)
+export class WaitingRoomResolver {
+  /*************************************
+                    QUERY
+     *************************************/
+
+  @Query(() => [WaitingRoom])
+  async getAllWaitingRooms(): Promise<WaitingRoom[]> {
+    return await dataSource.getRepository(WaitingRoom).find();
+  }
+  /*************************************
+                   MUTATION
+     *************************************/
+
+  @Mutation(() => WaitingRoom)
+  async createWaitingRoom(
+    @Arg('data') data: WaitingRoomInput
+  ): Promise<WaitingRoom> {
+    return await dataSource.getRepository(WaitingRoom).save(data);
+  }
+}
