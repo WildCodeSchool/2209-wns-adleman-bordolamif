8650321192279c7f2ea5361a324f2d82b4d43fa2
Sandra VOISIN <sandra.voisin92@gmail.com>
2022-11-18 10:01:44 +0100

    add Waiting Room CRUD

--- server/src/resolvers/WaitingRoomResolver.ts
+++ server/src/resolvers/WaitingRoomResolver.ts
@@ -1,6 +1,7 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Authorized, Int, Mutation, Query, Resolver } from 'type-graphql';
 import WaitingRoom, { WaitingRoomInput } from '../entity/WaitingRoom';
 import dataSource from '../db';
+import { ApolloError } from 'apollo-server-errors';
 
 @Resolver(WaitingRoom)
 export class WaitingRoomResolver {
@@ -16,10 +17,33 @@ export class WaitingRoomResolver {
                    MUTATION
      *************************************/
 
+  // @Authorized<Role>(['admin'])
   @Mutation(() => WaitingRoom)
   async createWaitingRoom(
     @Arg('data') data: WaitingRoomInput
   ): Promise<WaitingRoom> {
     return await dataSource.getRepository(WaitingRoom).save(data);
+  }
+
+  @Mutation(() => WaitingRoom)
+  async deleteWaitingRoom(@Arg('id', () => Int) id: number): Promise<boolean> {
+    const { affected } = await dataSource.getRepository(WaitingRoom).delete(id);
+    if (affected === 0)
+      throw new ApolloError('Waiting room not found', 'NOT_FOUND');
+    return true;
+  }
+
+  @Mutation(() => WaitingRoom)
+  async updtateWilder(
+    @Arg('id', () => Int) id: number,
+    @Arg('data') { name }: WaitingRoomInput
+  ): Promise<WaitingRoom> {
+    const { affected } = await dataSource
+      .getRepository(WaitingRoom)
+      .update(id, { name });
+
+    if (affected === 0) throw new ApolloError('skill not found', 'NOT_FOUND');
+
+    return { id, name };
   }
 }
