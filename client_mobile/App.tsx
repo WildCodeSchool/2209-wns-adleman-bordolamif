5213ed2e8105e9bf0a509b26d2655408d846fd3a
Theo Micaletti <theo.micaletti@fasst.io>
2023-01-30 11:04:46 +0100

    typage

--- client_mobile/App.tsx
+++ client_mobile/App.tsx
@@ -2,14 +2,15 @@ import HomeScreen from './src/screen/HomeScreen';
 import QrCodeScanner from './src/screen/QrCodeScanner';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList } from './src/types/RootStackParamList';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="QrCodeScanner" component={QrCodeScanner} />
       </Stack.Navigator>
     </NavigationContainer>
--- client_mobile/src/screen/HomeScreen.tsx
+++ client_mobile/src/screen/HomeScreen.tsx
@@ -1,15 +1,20 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import {
   Button, StyleSheet, Text, View,
 } from 'react-native';
+import { RootStackParamList } from '../types/RootStackParamList';
+
+type NavigationProps = NativeStackScreenProps<RootStackParamList, 'HomeScreen'>;
 
 // eslint-disable-next-line
-export default function HomeScreen({ navigation } : {navigation: any}) {
+export default function HomeScreen({ navigation }: NavigationProps) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>En construction</Text>
       <Button
         title="Go to QrCodeScanner"
+        // eslint-disable-next-line
         onPress={() => navigation.navigate('QrCodeScanner')}
       />
       <StatusBar style="auto" />
