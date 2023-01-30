43bc88c25e5f9fd49ae7ea91638721a09557c2e4
Joffrey Chevalier <100203396+JoffreyChevalier@users.noreply.github.com>
2023-01-30 15:26:48 +0100

    feature/login_mobile_app/50 (#53)
    * add login and logout in client_mobile
    
    * extract login from HomeScreen.tsx to LoginScreen.tsx
    
    * merged with QrCodeScanner branch
    
    * merged with login client_web

--- client_mobile/App.tsx
+++ client_mobile/App.tsx
@@ -3,17 +3,23 @@ import QrCodeScanner from './src/screen/QrCodeScanner';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from './src/types/RootStackParamList';
+import LoginScreen from './src/screen/LoginScreen';
+import { ApolloProvider } from '@apollo/client';
+import client from './graphQL/client';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="QrCodeScanner" component={QrCodeScanner} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="QrCodeScanner" component={QrCodeScanner} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
 
