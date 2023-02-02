34a82043ed5cfab54708ec50f4f129da84500743
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-02-02 09:42:55 +0100

    review correction

--- server/src/utils/types/InputTypes.ts
+++ server/src/utils/types/InputTypes.ts
@@ -70,6 +70,10 @@ export class UserInput {
 
 @InputType()
 export class TicketInput {
+  @Field({ nullable: true })
+  @MaxLength(100)
+    name?: string;
+
     @Field({ nullable: true })
       calledAt?: Date;
 
