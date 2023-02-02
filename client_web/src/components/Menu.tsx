12f4972d7cd69e6daa8c4531546e9a144d3aa123
SandraVsn <101800738+SandraVsn@users.noreply.github.com>
2023-02-02 17:24:59 +0100

    Add style to layout (#63)

--- client_web/src/components/Menu.tsx
+++ client_web/src/components/Menu.tsx
@@ -3,28 +3,29 @@ import { NavLink } from 'react-router-dom';
 function Menu({ currentUser }:any) {
   return (
     <div> {currentUser !== undefined && (
-    <div>
-      <h1>{currentUser.profile.email}</h1>
+    <div className="m-5 p-5 w-full">
       {currentUser.profile.role === 1 ? (
         <div>
-          <ul className="flex flex-col">
-            <NavLink to="/admin">Tableau de bord</NavLink>
-            <NavLink to="/admin/services">Gérer les services</NavLink>
-            <NavLink to="/admin/counters">Gérer les guichets</NavLink>
-            <NavLink to="/admin/statistics">Statistiques</NavLink>
+          <h1 className="pb-5">Admin {currentUser.profile.email}</h1>
+          <ul className="flex flex-col pb-5">
+            <NavLink className="pb-2" to="/admin">Tableau de bord</NavLink>
+            <NavLink className="pb-2" to="/admin/services">Gérer les services</NavLink>
+            <NavLink className="pb-2" to="/admin/counters">Gérer les guichets</NavLink>
+            <NavLink className="pb-2" to="/admin/statistics">Statistiques</NavLink>
           </ul>
-          <div>
-            <NavLink to="/admin/users">Gérer les opérateurs</NavLink>
+          <div className="bg-white p-2 rounded -ml-2">
+            <NavLink className="text-orange-500" to="/admin/users">Opérateurs</NavLink>
             <ul className="flex flex-col">
-              <NavLink to="/admin/users/create">Créer un opérateur</NavLink>
-              <NavLink to="/admin/users/update">Modifier un opérateur</NavLink>
-              <NavLink to="/admin/users/delete">Supprimer un opérateur</NavLink>
+              <NavLink className="py-2 pl-3" to="/admin/users/create">Créer un opérateur</NavLink>
+              <NavLink className="pb-2 pl-3" to="/admin/users/update">Modifier un opérateur</NavLink>
+              <NavLink className="pb-2 pl-3" to="/admin/users/delete">Supprimer un opérateur</NavLink>
             </ul>
           </div>
         </div>
       )
         : (
           <div>
+            <h1>Opérateur {currentUser.profile.email}</h1>
             <ul>
               <NavLink to="/operator">Mise en service</NavLink>
               <NavLink to="/operator/board">Tableau de bord</NavLink>
