b9729ee5a00ed296df4a867982e63eff3f426d50
Joffrey Chevalier <chevalier.joffrey@live.fr>
2023-02-27 12:36:25 +0100

    add test directory and test environment

---
+++ test/src/user.test.ts
@@ -0,0 +1,16 @@
+// import { it } from '@jest/globals';
+//
+// describe('User', () => {
+//   describe('Create', () => {
+//     it('should create a user', async () => {
+//     });
+//     it('should not create a user with an existing email', async () => {
+//     });
+//     describe('Validation', () => {
+//       it('should not create a user with an invalid email', async () => {
+//       });
+//       it('should not create a user with an invalid password', async () => {
+//       });
+//     });
+//   });
+// });
