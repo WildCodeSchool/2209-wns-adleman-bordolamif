033fa5af7fbe749be57b0cad934e6d137b872b10
Theo Micaletti <theo.micaletti@fasst.io>
2023-02-27 17:18:03 +0100

    refonte connexion admin

--- client_web/tailwind.config.cjs
+++ client_web/tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
     extend: {},
+    fontFamily: {
+      nunitobold: ['Nunito-bold', 'sans-serif'],
+    },
   },
   plugins: [],
-}
+};
