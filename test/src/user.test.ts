c5f12dc1af6b52b177c606717bd7b354b33139d7
Joffrey Chevalier <chevalier.joffrey@live.fr>
2023-02-27 16:18:37 +0100

    add first test for createUser

--- test/src/user.test.ts
+++ test/src/user.test.ts
@@ -1,16 +1,44 @@
-// import { it } from '@jest/globals';
-//
-// describe('User', () => {
-//   describe('Create', () => {
-//     it('should create a user', async () => {
-//     });
-//     it('should not create a user with an existing email', async () => {
-//     });
-//     describe('Validation', () => {
-//       it('should not create a user with an invalid email', async () => {
-//       });
-//       it('should not create a user with an invalid password', async () => {
-//       });
-//     });
-//   });
-// });
+import { it } from '@jest/globals';
+import { gql } from '@apollo/client';
+import client from './apolloClient';
+
+const createUserMutation = gql`
+    mutation CreateUser($data: UserInput!) {
+        createUser(data: $data) {
+            id
+            role
+            lastname
+            firstname
+            email
+            services {
+                id
+                color
+                acronym
+                name
+                open
+            }
+        }
+    }
+`;
+
+describe('User Resolver', () => {
+  describe('Create User', () => {
+    it('should create a user with the corresponding values', async () => {
+      const res = await client.mutate({
+        mutation: createUserMutation,
+        variables: {
+          data: {
+            role: 2, lastname: 'Doe', firstname: 'John', email: 'johndoe@fake.fr',
+          },
+        },
+      });
+
+      expect(res.data?.createUser).toHaveProperty('id');
+      expect(res.data?.createUser).toHaveProperty('role', 2);
+      expect(res.data?.createUser).toHaveProperty('lastname', 'Doe');
+      expect(res.data?.createUser).toHaveProperty('firstname', 'John');
+      expect(res.data?.createUser).toHaveProperty('email', 'johndoe@fake.fr');
+      expect(res.data?.createUser).toHaveProperty('services', []);
+    });
+  });
+});
