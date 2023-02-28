96ca9a098e7d4df244bc1d33ccec1f6520d50734
Axel Cabanat <axelcabanat@gmail.com>
2023-02-28 17:12:00 +0100

    start tests

--- test/src/user.test.ts
+++ test/src/user.test.ts
@@ -23,7 +23,7 @@ const createUserMutation = gql`
 
 describe('User Resolver', () => {
   describe('Create User', () => {
-    it('should create a user with the corresponding values', async () => {
+    it('should create an operator with the corresponding values', async () => {
       const res = await client.mutate({
         mutation: createUserMutation,
         variables: {
@@ -39,6 +39,36 @@ describe('User Resolver', () => {
       expect(res.data?.createUser).toHaveProperty('firstname', 'John');
       expect(res.data?.createUser).toHaveProperty('email', 'johndoe@fake.fr');
       expect(res.data?.createUser).toHaveProperty('services', []);
+    });
+
+    it('should create an admin with the corresponding values', async () => {
+      const res = await client.mutate({
+        mutation: createUserMutation,
+        variables: {
+          data: {
+            role: 1, lastname: 'Doe', firstname: 'John', email: 'johndoe@fake.fr', password: 'P4$$W0rd',
+          },
+        },
+      });
+
+      expect(res.data?.createUser).toHaveProperty('id');
+      expect(res.data?.createUser).toHaveProperty('role', 1);
+      expect(res.data?.createUser).toHaveProperty('lastname', 'Doe');
+      expect(res.data?.createUser).toHaveProperty('firstname', 'John');
+      expect(res.data?.createUser).toHaveProperty('email', 'johndoe@fake.fr');
+      expect(res.data?.createUser).toHaveProperty('services', []);
+    });
+
+    it('should throw an error', async () => {
+      const res = await client.mutate({
+        mutation: createUserMutation,
+        variables: {
+          data: {
+            role: 1, lastname: 'Doe', firstname: 'John', email: 'johndoe@fake.fr',
+          },
+        },
+      });
+      expect(res.errors).rejects.toThrow();
     });
   });
 });
