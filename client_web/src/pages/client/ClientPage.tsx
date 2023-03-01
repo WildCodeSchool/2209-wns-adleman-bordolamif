3d4cba55dd8a7522cc054751ea61742ebe73da36
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-03-01 17:10:08 +0100

    init Client Page

--- client_web/src/pages/client/ClientPage.tsx
+++ client_web/src/pages/client/ClientPage.tsx
@@ -3,9 +3,9 @@ import { GET_ALL_SERVICES } from '@graphQL/query/serviceQuery';
 
 function ClientPage() {
   const {
-    loading: servicesListLoading,
+    // loading: servicesListLoading,
     data: servicesList,
-    refetch: refetchServicesList,
+    // refetch: refetchServicesList,
   } = useQuery(GET_ALL_SERVICES);
 
   return (
