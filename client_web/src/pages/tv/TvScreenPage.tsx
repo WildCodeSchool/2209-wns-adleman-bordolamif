0f4798c6bbe05bd03bef11c587e3eda6e75ea80c
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-03-28 14:05:00 +0200

    wip

--- client_web/src/pages/tv/TvScreenPage.tsx
+++ client_web/src/pages/tv/TvScreenPage.tsx
@@ -1,14 +1,14 @@
 function TvScreenPage() {
   return (
     <div className="flex flex-row">
-        <div className="bg-gray-800 text-white">
-            <h2>Tickets Appelés</h2>
-            <p>Merci de vous rendre au guichet indiqué</p>
-        </div>
+      <div className="bg-gray-800 text-white">
+        <h2>Tickets Appelés</h2>
+        <p>Merci de vous rendre au guichet indiqué</p>
+      </div>
 
-        <div>
-            <h2>Prochains tickets en attente</h2>
-        </div>
+      <div>
+        <h2>Prochains tickets en attente</h2>
+      </div>
     </div>
   );
 }
