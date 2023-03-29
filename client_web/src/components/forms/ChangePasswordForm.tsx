d770ef6e176f51861afbae6b4d4716bea8fed606
Theo-micaletti <112871106+Theo-micaletti@users.noreply.github.com>
2023-03-29 11:12:25 +0200

    Tailwind classes components (#158)
    * color modifications for services and connexion interface rework (only visual exept for disabled)
    
    * color modifications for services and connexion interface rework (only visual exept for disabled)
    
    * color modifications for services and connexion interface rework (only visual exept for disabled)
    
    * color modifications for services and connexion interface rework (only visual exept for disabled)
    
    * resolving flex-raw conflicts
    
    * replace flex-raw with flex-row
    
    * classes for buttons
    
    * tailwind classes for buttons, inputs and titles
    
    * adding new tailwind classes
    
    * adding new tailwind classes
    
    * update and delete icons
    
    * update and delete icons

--- client_web/src/components/forms/ChangePasswordForm.tsx
+++ client_web/src/components/forms/ChangePasswordForm.tsx
@@ -32,7 +32,7 @@ function ChangePasswordForm(props:Props) {
         <EyeSlashIcon {...iconProps} />
       )}
       <div className="relative">
-        <LockClosedIcon className="absolute left-2 top-2 w-6 text-orange-500" />
+        <LockClosedIcon className="f-auth-icon" />
         <input
           type={showPassword ? 'text' : 'password'}
           placeholder="Nouveau mot de passe"
@@ -41,7 +41,7 @@ function ChangePasswordForm(props:Props) {
         />
       </div>
       <div className="relative">
-        <LockClosedIcon className="absolute left-2 top-2 w-6 text-orange-500" />
+        <LockClosedIcon className="f-auth-icon" />
         <input
           type={showPassword ? 'text' : 'password'}
           placeholder="Verifier le mot de passe"
@@ -52,7 +52,7 @@ function ChangePasswordForm(props:Props) {
       {error && <div className="text-red-600">{error}</div>}
       <button
         type="submit"
-        className="drop-shadow bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 rounded w-3/6"
+        className="f-button-green"
       >
         Valider
       </button>
--- client_web/src/pages/admin/AdminPage.tsx
+++ client_web/src/pages/admin/AdminPage.tsx
@@ -1,7 +1,8 @@
 function AdminPage() {
   return (
-    <div>
-      <p>ADMIN</p>
+    <div className="f-title-format">
+      <h1 className="f-main-title">Tableau de bord</h1>
+      <div className="f-decoration-line" />
     </div>
 
   );
