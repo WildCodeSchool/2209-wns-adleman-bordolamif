2bf33906f608eb3ef2c3894c0626f0537d888feb
Joffrey Chevalier <chevalier.joffrey@live.fr>
2023-04-24 15:59:27 +0200

    installed and configured nativewind

--- client_mobile/src/screen/HomeScreen.tsx
+++ client_mobile/src/screen/HomeScreen.tsx
@@ -1,16 +1,14 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import {
-  Button, StyleSheet, Text, View,
-} from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { RootStackParamList } from '../types/RootStackParamList';
 
 type NavigationProps = NativeStackScreenProps<RootStackParamList, 'HomeScreen'>;
 
 export default function HomeScreen({ navigation }: NavigationProps) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>En construction</Text>
+    <View className="">
+      <Text className="text-red-600 text-5xl">En construction</Text>
       <View style={{ marginBottom: 20 }}>
         <Button
           title="Login"
@@ -25,20 +23,3 @@ export default function HomeScreen({ navigation }: NavigationProps) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 30,
-    height: 80,
-    width: 300,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    borderRadius: 10,
-  },
-});
