f429cd331578dd69653afafdf79bb52f8bcc3ef9
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-05-22 11:53:43 +0200

    fix: expose stats functions in a specific file

--- client_web/src/components/adminComponents/AdminStatistics.tsx
+++ client_web/src/components/adminComponents/AdminStatistics.tsx
@@ -4,7 +4,14 @@ import TicketsByServicesChart from '@components/charts/TicketsByServicesChart';
 import TicketsPerDayChart from '@components/charts/TicketsPerDayChart';
 import DateTimePicker from '@components/utils/DateTimePicker';
 import { GET_ALL_TICKETS_BETWEEN_TWO_DATES } from '@graphQL/query/ticketQuery';
-import { TicketData } from '@utils/types/DataTypes';
+import {
+  attendanceByService,
+  averageWaitingTime,
+  averageWaitingTimePerService,
+  mostPupularService,
+  percentageOfReturnedTickets,
+  ticketsPerDay,
+} from '@utils/statistics/statFunctions';
 import { StartEndDate } from '@utils/types/InputTypes';
 import { useState } from 'react';
 
@@ -19,123 +26,6 @@ function AdminStatistics() {
     getAllTicketsBetweenTwoDates({ variables: { data: dates } });
   };
 
-  const averageWaitingTime = () => {
-    let totalWaitingTime = 0;
-    let numCalledTickets = 0;
-
-    for (const ticket of ticketList.getAllTicketsBetweenTwoDates) {
-      if (ticket.calledAt) {
-        const waitTime = new Date(ticket.calledAt).getTime() - new Date(ticket.createdAt).getTime();
-        totalWaitingTime += waitTime;
-        numCalledTickets += 1;
-      }
-    }
-
-    return Math.round(totalWaitingTime / (numCalledTickets * 60000));
-  };
-
-  const mostPupularService = () => {
-    const serviceCounts:{[serviceName:string]:number} = {};
-    ticketList.getAllTicketsBetweenTwoDates.forEach((ticket:TicketData) => {
-      const serviceName = ticket.service.name;
-      serviceCounts[serviceName] = serviceCounts[serviceName] || 0;
-      serviceCounts[serviceName] += 1;
-    });
-    const maxServiceCount = Math.max(...Object.values(serviceCounts));
-    return Object.keys(serviceCounts)
-      .find((serviceName) => serviceCounts[serviceName] === maxServiceCount);
-  };
-
-  const percentageOfReturnedTickets = () => {
-    const returnedCount = ticketList.getAllTicketsBetweenTwoDates
-      .filter((ticket:TicketData) => ticket.isReturned).length;
-
-    return ((returnedCount / ticketList.getAllTicketsBetweenTwoDates.length) * 100).toFixed();
-  };
-
-  const attendanceByService = () => {
-    const servicesData: { [serviceId: number]
-        : { name: string; color: string; count: number } } = {};
-    if (ticketList) {
-      ticketList.getAllTicketsBetweenTwoDates.forEach((ticket:TicketData) => {
-        const serviceId = ticket.service.id;
-        if (!servicesData[serviceId]) {
-          servicesData[serviceId] = {
-            name: ticket.service.name,
-            color: ticket.service.color,
-            count: 1,
-          };
-        } else {
-          servicesData[serviceId].count += 1;
-        }
-      });
-    }
-    const chartData = Object.values(servicesData).map((serviceData) => ({
-      name: serviceData.name,
-      color: serviceData.color,
-      ticketsNb: serviceData.count,
-    }));
-    return chartData;
-  };
-
-  const ticketsPerDay = () => {
-    const startDate = new Date(dateInterval!.startDate);
-    const endDate = new Date(dateInterval!.endDate);
-    const result = [];
-
-    while (startDate <= endDate) {
-      const currentDate = startDate.getDate().toString().padStart(2, '0');
-      const currentMonth = (startDate.getMonth() + 1).toString().padStart(2, '0');
-      const currentYear = startDate.getFullYear().toString();
-      const currentDateString = `${currentDate}-${currentMonth}-${currentYear}`;
-
-      const ticketsOnCurrentDate = ticketList.getAllTicketsBetweenTwoDates.filter(
-        (ticket: TicketData) => ticket.createdAt.substring(0, 10) === currentDateString.split('-').reverse().join('-'),
-      );
-      const ticketsNb = ticketsOnCurrentDate.length;
-      result.push({ date: currentDateString, ticketsNb });
-      startDate.setDate(startDate.getDate() + 1);
-    }
-
-    return result;
-  };
-
-  const averageWaitingTimePerService = () => {
-    const servicesMap = new Map<number, { name: string; color: string; waitingTimes: number[] }>();
-
-    for (const ticket of ticketList.getAllTicketsBetweenTwoDates) {
-      if (ticket.calledAt != null) {
-        const serviceId = ticket.service.id;
-        const service = servicesMap.get(serviceId);
-        const waitingTime = (new Date(ticket.calledAt).getTime()
-        - new Date(ticket.createdAt).getTime()) / (1000 * 60);
-
-        if (service) {
-          service.waitingTimes.push(waitingTime);
-        } else {
-          servicesMap.set(serviceId, {
-            name: ticket.service.name,
-            color: ticket.service.color,
-            waitingTimes: [waitingTime],
-          });
-        }
-      }
-    }
-    const chartData = [];
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    for (const [serviceId, service] of servicesMap.entries()) {
-      const waitingTimes = service.waitingTimes.length ? service.waitingTimes : [0];
-      const waitingTime = ((waitingTimes.reduce((acc, time) => acc + time, 0))
-      / waitingTimes.length).toFixed();
-      chartData.push({
-        name: service.name,
-        color: service.color,
-        waitingTime: parseInt(waitingTime, 10),
-      });
-    }
-    return chartData;
-  };
-
   return (
     <div>
       <div>
@@ -148,27 +38,37 @@ function AdminStatistics() {
                 <p>Tickets traités</p>
               </div>
               <div className="bg-orange-600 text-white rounded-2xl p-3 m-1">
-                <p>{ticketList && averageWaitingTime()} minutes</p>
+                <p>{ticketList
+                && averageWaitingTime(ticketList.getAllTicketsBetweenTwoDates)} minutes
+                </p>
                 <p>Temps d'attente moyen</p>
               </div>
               <div className="bg-orange-700 text-white rounded-2xl p-3 m-1">
-                <p>{ticketList && mostPupularService()}</p>
+                <p>{ticketList && mostPupularService(ticketList.getAllTicketsBetweenTwoDates)}</p>
                 <p>Service le plus fréquenté</p>
               </div>
               <div className="bg-orange-600 text-white rounded-2xl p-3 m-1">
-                <p>{ticketList && percentageOfReturnedTickets()} %</p>
+                <p>{ticketList
+                && percentageOfReturnedTickets(ticketList.getAllTicketsBetweenTwoDates)} %
+                </p>
                 <p>Tickets ajournés</p>
               </div>
             </div>
             <div>
               <p>Nombre de ticket total</p>
-              <TicketsByServicesChart chartData={attendanceByService()} />
+              <TicketsByServicesChart
+                chartData={attendanceByService(ticketList.getAllTicketsBetweenTwoDates)}
+              />
             </div>
             <div>
-              <TicketsPerDayChart chartData={ticketsPerDay()} />
+              <TicketsPerDayChart
+                chartData={ticketsPerDay(dateInterval!, ticketList.getAllTicketsBetweenTwoDates)}
+              />
             </div>
             <div>
-              <AverageWaitingTimePerService chartData={averageWaitingTimePerService()} />
+              <AverageWaitingTimePerService
+                chartData={averageWaitingTimePerService(ticketList.getAllTicketsBetweenTwoDates)}
+              />
             </div>
           </div>
         ) : <div>Veuillez selectionner la période à afficher</div>}
---
+++ client_web/src/utils/statistics/statFunctions.ts
@@ -0,0 +1,119 @@
+import { TicketData } from '@utils/types/DataTypes';
+import { StartEndDate } from '@utils/types/InputTypes';
+
+export const averageWaitingTime = (ticketList: TicketData[]) => {
+  let totalWaitingTime = 0;
+  let numCalledTickets = 0;
+
+  for (const ticket of ticketList) {
+    if (ticket.calledAt) {
+      const waitTime = new Date(ticket.calledAt).getTime() - new Date(ticket.createdAt).getTime();
+      totalWaitingTime += waitTime;
+      numCalledTickets += 1;
+    }
+  }
+
+  return Math.round(totalWaitingTime / (numCalledTickets * 60000));
+};
+
+export const mostPupularService = (ticketList: TicketData[]) => {
+  const serviceCounts:{[serviceName:string]:number} = {};
+  ticketList.forEach((ticket:TicketData) => {
+    const serviceName = ticket.service.name;
+    serviceCounts[serviceName] = serviceCounts[serviceName] || 0;
+    serviceCounts[serviceName] += 1;
+  });
+  const maxServiceCount = Math.max(...Object.values(serviceCounts));
+  return Object.keys(serviceCounts)
+    .find((serviceName) => serviceCounts[serviceName] === maxServiceCount);
+};
+
+export const percentageOfReturnedTickets = (ticketList: TicketData[]) => {
+  const returnedCount = ticketList
+    .filter((ticket:TicketData) => ticket.isReturned).length;
+
+  return ((returnedCount / ticketList.length) * 100).toFixed();
+};
+
+export const attendanceByService = (ticketList: TicketData[]) => {
+  const servicesData: { [serviceId: number]
+        : { name: string; color: string; count: number } } = {};
+  if (ticketList) {
+    ticketList.forEach((ticket:TicketData) => {
+      const serviceId = ticket.service.id;
+      if (!servicesData[serviceId]) {
+        servicesData[serviceId] = {
+          name: ticket.service.name,
+          color: ticket.service.color,
+          count: 1,
+        };
+      } else {
+        servicesData[serviceId].count += 1;
+      }
+    });
+  }
+  const chartData = Object.values(servicesData).map((serviceData) => ({
+    name: serviceData.name,
+    color: serviceData.color,
+    ticketsNb: serviceData.count,
+  }));
+  return chartData;
+};
+
+export const ticketsPerDay = (dateInterval: StartEndDate, ticketList: TicketData[]) => {
+  const startDate = new Date(dateInterval!.startDate);
+  const endDate = new Date(dateInterval!.endDate);
+  const result = [];
+
+  while (startDate <= endDate) {
+    const currentDate = startDate.getDate().toString().padStart(2, '0');
+    const currentMonth = (startDate.getMonth() + 1).toString().padStart(2, '0');
+    const currentYear = startDate.getFullYear().toString();
+    const currentDateString = `${currentDate}-${currentMonth}-${currentYear}`;
+
+    const ticketsOnCurrentDate = ticketList.filter(
+      (ticket: TicketData) => ticket.createdAt.substring(0, 10) === currentDateString.split('-').reverse().join('-'),
+    );
+    const ticketsNb = ticketsOnCurrentDate.length;
+    result.push({ date: currentDateString, ticketsNb });
+    startDate.setDate(startDate.getDate() + 1);
+  }
+
+  return result;
+};
+
+export const averageWaitingTimePerService = (ticketList: TicketData[]) => {
+  const servicesMap = new Map<number, { name: string; color: string; waitingTimes: number[] }>();
+
+  for (const ticket of ticketList) {
+    if (ticket.calledAt != null) {
+      const serviceId = ticket.service.id;
+      const service = servicesMap.get(serviceId);
+      const waitingTime = (new Date(ticket.calledAt).getTime()
+        - new Date(ticket.createdAt).getTime()) / (1000 * 60);
+
+      if (service) {
+        service.waitingTimes.push(waitingTime);
+      } else {
+        servicesMap.set(serviceId, {
+          name: ticket.service.name,
+          color: ticket.service.color,
+          waitingTimes: [waitingTime],
+        });
+      }
+    }
+  }
+  const chartData = [];
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  for (const [serviceId, service] of servicesMap.entries()) {
+    const waitingTimes = service.waitingTimes.length ? service.waitingTimes : [0];
+    const waitingTime = ((waitingTimes.reduce((acc, time) => acc + time, 0))
+      / waitingTimes.length).toFixed();
+    chartData.push({
+      name: service.name,
+      color: service.color,
+      waitingTime: parseInt(waitingTime, 10),
+    });
+  }
+  return chartData;
+};
