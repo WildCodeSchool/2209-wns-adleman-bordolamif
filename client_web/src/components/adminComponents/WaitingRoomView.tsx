eb943860398bc394e787558deb32ddd56a256de7
Theo Micaletti <theo.micaletti@fasst.io>
2023-05-25 14:47:11 +0200

    :lipstick: waiting-room ui/ux in dashboard/admin

--- client_web/src/components/adminComponents/WaitingRoomView.tsx
+++ client_web/src/components/adminComponents/WaitingRoomView.tsx
@@ -15,31 +15,35 @@ function WaitingRoomView(props: Props) {
     <div>
       {waitingRoom.services.map((service) => (
         <div className="bg-gray-200 rounded-2xl m-2 p-2" key={service.id}>
-          <div className="flex flex-row items-center">
+          <div className="flex flex-row items-center mt-2 ml-2">
             <ServiceIcon service={service} />
-            <p className="ml-1">{service.name}</p>
+            <p className="ml-2 text-2xl">{service.name}</p>
           </div>
           <div className="flex flex-row">
-            <div className="bg-white rounded m-1 p-1">
-              <p>Etat du service</p>
-              <p>Tickets en attente : {ticketList && ticketList
+            <div className="bg-white rounded-2xl m-2 p-3 w-1/3">
+              <p className="text-xl nunito-bold">Etat du service</p>
+              <p className="text-lg">
+                Tickets en attente : {ticketList && ticketList
                 .filter((ticket:TicketData) => ticket.status
               === StatusEnum.EN_ATTENTE && ticket.service.id === service.id).length}
               </p>
-              <p>Tickets suspendus : {ticketList && ticketList
+              <p className="text-lg">
+                Tickets suspendus : {ticketList && ticketList
                 .filter((ticket:TicketData) => ticket.status === StatusEnum.AJOURNE
               && ticket.service.id === service.id).length}
               </p>
-              <p>Total : {ticketList && ticketList
+              <p className="text-lg">
+                Total : {ticketList && ticketList
                 .filter((ticket:TicketData) => ticket.status !== StatusEnum.TRAITE
               && ticket.service.id === service.id).length}
               </p>
-              <p>Guichets ouverts : {connectedUsersList
+              <p className="text-lg">
+                Guichets ouverts : {connectedUsersList
                 && connectedUsersList.filter((user) => user.currentService?.id
                 === service.id).length}
               </p>
             </div>
-            <div className="bg-white rounded">
+            <div className="bg-white rounded-2xl m-2 w-2/3">
               <OperatorsInService connectedUsersList={connectedUsersList
                 && connectedUsersList.filter((user) => user.currentService?.id
                 === service.id)}
