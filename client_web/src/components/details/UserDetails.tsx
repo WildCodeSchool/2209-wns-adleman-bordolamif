27e7ea6c7d92ab0cb17e72ff18b110ae5f842642
Theo Micaletti <theo.micaletti@fasst.io>
2023-05-26 15:07:13 +0200

    :bug: adding admin statistics and little suff

--- client_web/src/components/details/UserDetails.tsx
+++ client_web/src/components/details/UserDetails.tsx
@@ -16,25 +16,25 @@ function UserDetails(props: Props) {
     <div className="grid grid-cols-6 items-center mb-4">
       <p className="text-xl nunito-bold">{user.firstname}</p>
       <p className="text-xl nunito-bold">{user.lastname}</p>
-      <p className="">{user.email}</p>
+      <p className="text-sm">{user.email}</p>
       <button
         type="button"
         onClick={() => handleUpdateUserSuspension(!user.isSuspended, user.id)}
-        className="nunito-bold cursor-pointer"
+        className="nunito-bold cursor-pointer ml-10 bg-gray-100 hover:bg-gray-200 drop-shadow p-2 rounded-2xl"
       >
-        Suspendu : {user.isSuspended ? 'oui' : 'non'}
+        Suspendu : {user.isSuspended ? '✅' : '❌'}
       </button>
       <button
         type="button"
         onClick={() => handleOpenModal(user)}
-        className="f-button-green ml-8"
+        className="f-button-green ml-16"
       >
         Modifier
       </button>
       <button
         type="button"
         onClick={() => handleDeleteUser(user.id)}
-        className="f-button-red"
+        className="f-button-red ml-8"
       >
         Supprimer
       </button>
