7bbaa22b70d7efc7f64218b680886c22020edaf7
Axel Cabanat <axelcabanat@gmail.com>
2023-07-19 11:36:46 +0200

    add goToHomePage

--- client_mobile/src/components/WaitingInformationsCard.tsx
+++ client_mobile/src/components/WaitingInformationsCard.tsx
@@ -1,17 +1,20 @@
-import { Text, View } from 'react-native';
+import { Text, View, Pressable } from 'react-native';
 import React from 'react';
 import { TicketData, UpdatedTicketData } from '../types/DataTypes';
+import { StatusEnum } from '../../utils/enum/StatusEnum';
 
 interface Props {
   currentTicketIndex: number | undefined;
   currentTicket: TicketData ;
   calledTicketId: number;
   updatedTicket: UpdatedTicketData | undefined;
+  handleGoToHomePage: () => void;
 }
 function WaitingInformationsCard(props: Props) {
   const {
-    currentTicketIndex, currentTicket, calledTicketId, updatedTicket,
+    currentTicketIndex, currentTicket, calledTicketId, updatedTicket, handleGoToHomePage,
   } = props;
+
   return (
     <>
       {
@@ -49,7 +52,7 @@ function WaitingInformationsCard(props: Props) {
     currentTicketIndex! === 0 && (
       <View className="flex flex-col items-center justify-center">
         <View className="bg-gray-100 w-screen rounded-b-[50px] mt-5 pb-16">
-          <Text className="text-5xl text-center mx-2">La personne devant vous est au guichet</Text>
+          <Text className="text-5xl text-center mx-2">Vous êtes la première personne dans la file d'attente</Text>
         </View>
         <View className="-z-10 -top-10 bg-yellow-500 pt-20 pb-10 w-screen rounded-b-[50px]">
           <Text className="text-4xl font-bold text-white text-center mx-6">Vous allez bientôt être appelé</Text>
@@ -57,6 +60,24 @@ function WaitingInformationsCard(props: Props) {
       </View>
     )
   }
+      { currentTicketIndex === -1
+      && updatedTicket?.updatedTicketByServiceId!.status === StatusEnum.TRAITE && (
+        <View className="flex flex-col items-center justify-center">
+          <View className="bg-gray-100 w-screen rounded-b-[50px] mt-5 pb-24">
+            <Text className="text-2xl font-bold text-center mb-2">Votre demande a bien été traitée</Text>
+            <Text className="text-6xl text-center font-bold mt-5">Merci pour votre visite</Text>
+          </View>
+          <View className="-z-10 -top-10 bg-green-500 pt-24 pb-16 w-screen rounded-b-[50px]">
+            <Pressable className="text-5xl font-bold text-white text-center" onPress={handleGoToHomePage}>
+              <Text
+                style={{ textAlignVertical: 'center' }}
+                className="text-2xl font-bold text-center mb-2"
+              >Retour à l'accueil
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      )}
       {
     currentTicket.id === calledTicketId && (
       <View className="flex flex-col items-center justify-center">
