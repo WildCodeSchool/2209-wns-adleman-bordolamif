693b3ecc9b366919e8357c2e74f9905e1ca5635b
Axel Cabanat <axelcabanat@gmail.com>
2023-09-03 17:44:41 +0200

    remove previous message on ticket's screen

--- client_mobile/src/components/WaitingInformationsCard.tsx
+++ client_mobile/src/components/WaitingInformationsCard.tsx
@@ -20,13 +20,13 @@ function WaitingInformationsCard(props: Props) {
       {
       currentTicketIndex! > 3 && (
         <View className="flex flex-col justify-center">
-          <View className="bg-gray-100 w-screen rounded-b-[50px] mt-5 pb-16">
+          <View className="bg-gray-100 w-screen rounded-b-[50px] mt-[2vh] pb-[6vh]">
             <Text className="text-5xl text-center">Il y a</Text>
             <Text className="text-5xl text-center font-bold">{currentTicketIndex} <Text className="text-5xl text-center font-normal">personnes</Text></Text>
             <Text className="text-5xl text-center">devant vous</Text>
             <Text> </Text>
           </View>
-          <View className="-z-10 -top-10 bg-red-500 pt-24 pb-16 w-screen rounded-b-[50px]">
+          <View className="-z-10 -top-10 bg-red-500 pt-[12vh] pb-[6vh] w-screen rounded-b-[50px]">
             <Text className="text-4xl font-bold text-white text-center mx-2">Ce n'est pas votre tour</Text>
           </View>
         </View>
@@ -79,16 +79,17 @@ function WaitingInformationsCard(props: Props) {
         </View>
       )}
       {
-    currentTicket.id === calledTicketId && (
-      <View className="flex flex-col items-center justify-center">
-        <View className="bg-gray-100 w-screen rounded-b-[50px] mt-5 pb-24">
-          <Text className="text-2xl font-bold text-center mb-2">Veuillez vous rendre au</Text>
-          <Text className="text-6xl text-center font-bold mt-5">{ updatedTicket?.updatedTicketByServiceId.counter.name }</Text>
-        </View>
-        <View className="-z-10 -top-10 bg-green-500 pt-24 pb-16 w-screen rounded-b-[50px]">
-          <Text className="text-5xl font-bold text-white text-center">C'est à vous !</Text>
-        </View>
-      </View>
+    currentTicket.id === calledTicketId
+     && updatedTicket?.updatedTicketByServiceId!.status !== StatusEnum.TRAITE && (
+     <View className="flex flex-col items-center justify-center">
+       <View className="bg-gray-100 w-screen rounded-b-[50px] mt-5 pb-24">
+         <Text className="text-2xl font-bold text-center mb-2">Veuillez vous rendre au</Text>
+         <Text className="text-6xl text-center font-bold mt-5">{ updatedTicket?.updatedTicketByServiceId.counter.name }</Text>
+       </View>
+       <View className="-z-10 -top-10 bg-green-500 pt-24 pb-16 w-screen rounded-b-[50px]">
+         <Text className="text-5xl font-bold text-white text-center">C'est à vous !</Text>
+       </View>
+     </View>
     )
   }
     </>
